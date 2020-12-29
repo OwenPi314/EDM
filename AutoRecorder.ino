3344798cc3a0d1618bdeba7848f3ed977e982100
Zhiqiang Pi <owenpi3.1415922@gmail.com>
2020-12-28 21:10:54 -0800

    Semi-finised autorecorder

--- AutoRecorder.ino
+++ AutoRecorder.ino
@@ -2,22 +2,27 @@
 #include <SPI.h>
 #include <TMRpcm.h>
 
-#define SD_ChipSelectPin 10  // 如果使用 arduino nano 328 可使用Pin4
+#define SD_ChipSelectPin 10  
 
-TMRpcm audio;   
-boolean isRecording;
-int counter;
-char * fileName;
-int lastCheck = 0;
-boolean sound = false;
+//Declare variables
+TMRpcm audio;
+boolean isRecording; //whether the device is recording
+int counter;         //how many files have been recorded (used for file naming)
+char * fileName;     //the file to record to 
+int lastCheck = 0;   //when was the last check for if there is sound
+boolean sound;       //whether there is sound
 
-void setup() {
-  
+void setup() 
+{
   Serial.begin(115200);
-  
+
+  //Initialize variables
   isRecording = false;
   counter = 0;
   fileName = "";
+  sound = false;
+  
+  //Check SD card
   if (!SD.begin(SD_ChipSelectPin)) {  
     Serial.println("SD Fail");
     return;
@@ -25,76 +30,64 @@ void setup() {
     Serial.println("SD OK"); 
   }
   audio.CSPin = SD_ChipSelectPin;
+
+  //Wait for setup (maybe unnecessary)
   delay(1000);
 }
 
-
-
-
-void loop() {
-  if(sound == false)
+void loop() 
+{
+  if(!isRecording) //constantly check if a sentence has started when not recording
   {
     lastCheck = millis();
     sound = soundExist();
   }
-  else if(millis() - lastCheck > 1000)
+  else //during recording, constantly check if there has been no sound recorded for a second
   {
+    sound = false;
+    while(millis() - lastCheck < 1000)
+    {
+      sound = sound || soundExist();
+    }
     lastCheck = millis();
-    sound = soundExist();
   }
 
-  if(sound)
+  if(sound) //If there is sound and recording hasn't started, start recording
   {
     if(!isRecording)
     {
       startRecord();
     }
   }
-  else
+  else //if there is no sound and the device is recording, stop recording
   {
     if(isRecording)
-    {      
+    {     
       stopRecord();
     }
   }
 }
 
-
-
-
-
-
+//function to check whether there is input to the digital audio sensor
 boolean soundExist()
 {
-  boolean temp = false;
-
-  for(int i = 0; i < 500; i++)
-  {
-    boolean input = digitalRead(4) == 1 ? false : true;
-    
-    temp = (temp || input);
-    if(temp == true)
-    {
-      return temp;
-    }
-  }
-  return temp;
+  return (digitalRead(4) == 0);
 }
 
-
+//this function starts the recording
 void startRecord()
 {
   sprintf(fileName, "%i%s", counter, "audio.wav");
-  Serial.print("start ");
-  Serial.println(fileName);
+  Serial.println("start");
   isRecording = true;
   audio.startRecording(fileName, 16000, A0);
 }
 
+//this function stops the recording
 void stopRecord()
 {
-  Serial.print("stop ");
-  Serial.println(fileName);
+  Serial.println("stop");
+
   isRecording = false;
   counter++;
   audio.stopRecording(fileName);
